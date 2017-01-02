19cdfea977765cda8d6da3bf5e6729ebae032d8a
BlueKraken <gonzalo.tajmuch@gmail.com>
2017-01-02 00:01:07 -0300

    update movement controller
    mejoras en el script MovementController.cs. Implementado sistema de
    waypoints para el movimiento de la serpiente

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -13,11 +13,8 @@ public enum Direction
 
 public class MovementController : MonoBehaviour
 {
-
-    Rigidbody2D _rigidBody2D;
-
-    [SerializeField] //por mientras, deberia se manejado SOLO por detras
-    float _velocity;
+    public float Step;
+    public float SecondsPerMove;
 
     // para manejar cambios de direccion
     public Direction Direction;
@@ -30,6 +27,8 @@ public class MovementController : MonoBehaviour
     List<Vector2> _wayPoints;
     Vector2 _posicionAntCabeza;
 
+    string input;
+
     // Use this for initialization
     void Start()
     {
@@ -37,121 +36,112 @@ public class MovementController : MonoBehaviour
         _cuerpos = GameObject.Find("Cuerpos");
         _cola = GameObject.Find("Cola");
 
-        _rigidBody2D = GetComponent<Rigidbody2D>();
+        //Posicionar cola segun el primer cuerpo (el mas alejado de la cabeza)
+        _cola.transform.position = ListarCuerpos()[0].position - new Vector3(0,-0.64f,0);
+
         Direction = Direction.Up;
 
-        _wayPoints = new List<Vector2>() { _cola.transform.position };
+        // Inicializacion de los waypoints
+        _wayPoints = new List<Vector2>() { _cabeza.transform.position };
+
         foreach (Transform t in ListarCuerpos())
         {
             _wayPoints.Add(t.position);
         }
-        _wayPoints.Add(_cabeza.transform.position);
-        //_wayPoints.Reverse();
 
-        _posicionAntCabeza = _cabeza.transform.position;
+        _wayPoints.Add(_cola.transform.position);
+        _wayPoints.Reverse();
 
-        InvokeRepeating("Move", 1, 1f);
+        // Invocacion del Metodo 'Move' cada 'SecondsPerMove' segundos
+        InvokeRepeating("Move", 0, SecondsPerMove);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        // Agregar waypoint si hubo movimiento
-        //if((Vector3)_posicionAntCabeza != _cabeza.transform.position)
-        //{
-        //    _wayPoints.Add(_cabeza.transform.position);
-        //    _wayPoints.RemoveAt(0);
-        //}
-            
-
-        PreviousDirection = Direction;
-        //Move();
-
-        HandleInput();
+        // Si hubo input, se almacena hasta ser consumido
+        if(Input.anyKeyDown)
+            input = Input.inputString;
     }
 
     void HandleInput()
     {
         // Manejar cambio de Direccion
         //TODO: cambiar input a swipes
-        if (Input.GetKeyDown(KeyCode.W) && PreviousDirection != Direction.Down)
+        PreviousDirection = Direction;
+
+        if (input == "w" && Direction != Direction.Down)
             Direction = Direction.Up;
-        if (Input.GetKeyDown(KeyCode.D) && PreviousDirection != Direction.Left)
+
+        if (input == "d" && Direction != Direction.Left)
             Direction = Direction.Right;
-        if (Input.GetKeyDown(KeyCode.S) && PreviousDirection != Direction.Up)
+
+        if (input == "s" && Direction != Direction.Up)
             Direction = Direction.Down;
-        if (Input.GetKeyDown(KeyCode.A) && PreviousDirection != Direction.Right)
+
+        if (input == "a" && Direction != Direction.Right)
             Direction = Direction.Left;
 
         if (PreviousDirection != Direction)
-            RotateByDirection();
-        Move();
+            RotateByDirection(_cabeza.transform);
+
+        input = "";
     }
 
     void Move()
     {
+        HandleInput();
+
         Vector2 headDirection;
-        float step = _velocity * Time.deltaTime;
+
         switch (Direction)
         {
             case (Direction.Up):
-                headDirection = new Vector2(0, step);
+                headDirection = new Vector2(0, Step);
                 break;
             case (Direction.Left):
-                headDirection = new Vector2(-step, 0);
+                headDirection = new Vector2(-Step, 0);
                 break;
             case (Direction.Down):
-                headDirection = new Vector2(0, -step);
+                headDirection = new Vector2(0, -Step);
                 break;
             case (Direction.Right):
-                headDirection = new Vector2(step, 0);
+                headDirection = new Vector2(Step, 0);
                 break;
             default:
                 headDirection = new Vector2();
                 break;
         }
 
-        // Se mueve la cabeza
-        _posicionAntCabeza = _cabeza.transform.position;
+        // Mover la cabeza
         MoverObjeto(_cabeza, headDirection);
-
 
         int wpIndex = _wayPoints.Count - 1;
         List<Transform> listaDeCuerpos = ListarCuerpos();
+
         // Mover cada cuerpo en _cuerpos
         for (int i = 0; i <= listaDeCuerpos.Count - 1; i++)
         {
-            // Mover HACIA el siguiente waypoint
-
             // Obtener datos para calculo de direccion
             Vector2 posicionCuerpo = listaDeCuerpos[i].position;
             Vector2 posicionWayPoint = _wayPoints[wpIndex];
-            //Debug.Log("cuerpo "+i+" moviendose a waypoint "+wpIndex);
+            
             // Obtener el vector direccion
             Vector2 vectorDireccion = new Vector2(posicionWayPoint.x - posicionCuerpo.x, posicionWayPoint.y - posicionCuerpo.y);
-            // Factorizar la direccion segun el step
-            vectorDireccion *= step;
 
-            Debug.Log("elemento "+i+" sigue a "+posicionWayPoint);
-            PrintWayPoints();
             // Mover cuerpo
-            // FIX ME: 'listaDeCuerpos[i].position += (Vector3)vectorDireccion;' transform.position assign attempt for 'Cuerpo' is not valid. Input position is { -176267416332926980.000000, -Infinity, 0.000000 }. UnityEngine.Transform:set_position(Vector3) MovementController: Move()(at Assets / MovementController.cs:126)
-            // Probablemente un error de calculo al vector direccion
             MoverObjeto(listaDeCuerpos[i].gameObject, vectorDireccion);
-            //listaDeCuerpos[i].position += (Vector3)vectorDireccion;
-            // Mover el index de los waypoints
             wpIndex--;
 
         }
 
         // Mover cola
         Vector2 posicionCola = _cola.transform.position;
-        Vector2 posicionWayPointCola = _wayPoints[1];       // el waypoint de la cola siempre sera 1
+        Vector2 posicionWayPointCola = _wayPoints[1];       // el waypoint de la cola siempre sera 0, por lo que le sigue el 1
 
         Vector2 vDireccionCola = new Vector2(posicionWayPointCola.x - posicionCola.x, posicionWayPointCola.y - posicionCola.y);
-        vDireccionCola *= step;
+        //vDireccionCola *= step;
         MoverObjeto(_cola, vDireccionCola);
         //_cola.transform.position += (Vector3)vDireccionCola;
 
@@ -160,35 +150,41 @@ public class MovementController : MonoBehaviour
 
         //eliminar primer waypoint
         _wayPoints.RemoveAt(0);
-
-
-        //Debug.Log("cola sigue a " + _wayPoints[1].ToString());
     }
 
     void MoverObjeto(GameObject go, Vector2 direccion)
     {
-        var rigidBody2dCabeza = go.GetComponent<Rigidbody2D>();
-        rigidBody2dCabeza.velocity = direccion;
+        go.transform.position += (Vector3)direccion;
     }
 
 
     void MoverCuerpo()
     {
-
     }
 
 
-    void RotateByDirection()
+    void RotateByDirection(Transform t)
     {
         Quaternion q = new Quaternion();
         int rotation = GetAngleByDirection(Direction);
 
         q.eulerAngles = new Vector3(0, 0, rotation);
-        //para rotar el cuerpo entero
-        //transform.rotation = q;
-        //para rotar solo la cabeza
-        var cabeza = GameObject.Find("Cabeza");
-        cabeza.transform.rotation = q;
+        t.rotation = q;
+    }
+
+    List<Transform> ListarCuerpos()
+    {
+        List<Transform> lista = new List<Transform>();
+
+        for (int i = 0; i <= _cuerpos.transform.childCount - 1; i++)
+        {
+            // Si el GameObject es uno activo en la jerarquía de la escena
+            if (_cuerpos.transform.GetChild(i).gameObject.activeInHierarchy)
+            {
+                lista.Add(_cuerpos.transform.GetChild(i));
+            }
+        }
+        return lista;
     }
 
     //helper
@@ -216,21 +212,6 @@ public class MovementController : MonoBehaviour
         return angle;
     }
 
-    List<Transform> ListarCuerpos()
-    {
-        List<Transform> lista = new List<Transform>();
-
-        for (int i = 0; i <= _cuerpos.transform.childCount - 1; i++)
-        {
-            // Si el GameObject es uno activo en la jerarquía de la escena
-            if (_cuerpos.transform.GetChild(i).gameObject.activeInHierarchy)
-            {
-                lista.Add(_cuerpos.transform.GetChild(i));
-            }
-        }
-        return lista;
-    }
-
     //helper
     void PrintWayPoints()
     {
@@ -240,6 +221,11 @@ public class MovementController : MonoBehaviour
             s += v.ToString() + " ";
         }
         Debug.Log(s);
+    }
+
+    public void AddWaypoint(Vector2 pos)
+    {
+        _wayPoints.Add(pos);
     }
 }
 
